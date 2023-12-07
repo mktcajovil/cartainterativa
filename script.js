684983d97573f22c462a318f4e4a869509266f77
mktcajovil <153095819+mktcajovil@users.noreply.github.com>
2023-12-07 16:52:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-document.getElementById('envelope').addEventListener('click', function() {
-  document.getElementById('carta').style.display = 'block';
-});
-document.getElementById('envelope').addEventListener('click', function() {
-  this.classList.add('clicked');
-});
+
